b471a01b20dc0fc4366a752cc854b1ef69f330c7
Jaewoong <dixk3458@naver.com>
2023-12-19 17:04:28 +0900

    MarkdownViewer 스타일링

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -14,20 +14,22 @@ export default async function Post({ params }: Props) {
     params.slug
   );
   return (
-    <article>
+    <article className="rounded-2xl overflow-hidden m-4 bg-sky-50 shadow-lg">
       <Image
+        className="w-full h-1/5 max-h-[400px]"
         src={`/images/posts/${path}.png`}
         alt={title}
         width={760}
         height={420}
       />
-      <section>
-        <div>
+      <section className="flex flex-col p-4">
+        <div className="self-end flex items-center gap-2 font-bold text-gray-600">
           <FaRegCalendarAlt />
           <p>{date}</p>
         </div>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className='text-xl font-bold'>{description}</p>
+        <div className='w-44 border-2 border-gray-600 mt-4 mb-8'></div>
         <MarkdownViewer content={content} />
       </section>
     </article>
